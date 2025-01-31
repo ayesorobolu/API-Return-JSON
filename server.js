5e4e670ee54cbcd53d6291655632bc371771a22b
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-01-31 01:45:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get( "/", (req, res) => {
+res.json({
+email:"abrahamayesoro@gmail.com",
+current_datetime: new Date().toISOString(),
+github_url: ""
+})
+})
+
+app.listen(6000, () => {
+    console.log("Server running on http://localhost: 6000")
+});
